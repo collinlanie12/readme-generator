963736f61c224cc78d3df2c05a5cded9e555f834
Collin Lanier <collinlanier12@gmail.com>
2020-08-25 01:34:10 -0400

    Added different licenses, updated readme generator.

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const emoji = require('node-emoji');
 const terminalImage = require('terminal-image');
 const repoName = require('git-repo-name');
 const generateMarkdown = require('./utils/generateMarkdown');
+
 let readMeString;
 
 //emoji list
 const exclaim = emoji.get('exclamation');
 const describe = emoji.get('spiral_note_pad');
+const arrowDown = emoji.get('arrow_down');
 
 
 inquirer
     .prompt([
         {
             type: "input",
-            message: exclaim + " What is the Title?",
+            message: exclaim + " What is the title of your project?",
             name: "title",
             default: repoName.sync(),
 
@@ -27,7 +29,41 @@ inquirer
             default: "This is the readme for my " + repoName.sync() + " project!",
         },
         {
-
+            type: "input",
+            message: arrowDown + "  What should you run to install all dependencies?",
+            name: "install",
+            default: "npm i",
+        },
+        {
+            type: "input",
+            message: "What is the usage of your project?",
+            name: "usage",
+            default: "To enjoy!",
+        },
+        {
+            type: "list",
+            message: "What type of license will you be using?",
+            choices: ["MIT", "GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "ISC"],
+            name: "license",
+            default: "MIT",
+        },
+        {
+            type: "input",
+            message: "What is your full name?",
+            name: "fullName",
+            default: "John Doe",
+        },
+        {
+            type: "input",
+            message: "What year is it?",
+            name: "year",
+            default: "2020",
+        },
+        {
+            type: "input",
+            message: "Write all contributors, or just your name",
+            name: "contributors",
+            default: "John Doe",
         }
     ])
     .then(answers => {
